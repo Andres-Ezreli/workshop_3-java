671ec0faea9628f83cffc69d86b80d4ab34e1037
Andres <andres.workvault@gmail.com>
2025-10-18 14:38:39 -0500

    done

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("--- employer ---");
+        {
             Curso java101 = new Curso("Java 101");
             Profesor prof = new Profesor("Ana Perez", java101, 500.0);
             Administrador admin = new Administrador("Luis Gomez", "Recursos Humanos", 400.0);
 
             Nomina.generarPago(prof);
             Nomina.generarPago(admin);
-
+        }
         System.out.println("\n--- figures ---");
         {
             double[][] examples = new double[][]{
